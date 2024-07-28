9efedae80cb739e054b8d4ac6d5bea7dabf6939c
Shiva Kashyap <shiva.kashyap0008@gmail.com>
2024-07-28 12:00:16 +0530

    written api for find and get user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controller/userController");
+const { registerUser, loginUser, findUser, getUser } = require("../controller/userController");
 
 const router = express.Router()
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
+router.get("/find/:userId", findUser);
+router.get("/", getUser);
 module.exports = router
